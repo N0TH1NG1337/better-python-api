74441d7762979bb0d376c433200120b37e431475
N0TH1NG1337 <87660281+N0TH1NG1337@users.noreply.github.com>
2021-11-05 13:11:54 +0200

    Update better_api.py

--- better_api.py
+++ better_api.py
@@ -130,29 +130,22 @@ def table_sum(table_to_sum):
 #end
 
 def to_string(input_tostring):
-    #
-    if string_type(input_tostring) != "string":
-        return "invalid input"
-    else:
-        return str(input_tostring)
-    #end
+    
+    return str(input_tostring)
+    
 #end
 
 def to_int(input_toint):
-    if string_type(input_toint) != "int":
-        return "invalid input"
-    else:
-        return int(input_toint)
-    #end
+    
+    return int(input_toint)
+    
 #end
 
 def to_float(input_tofloat):
     #
-    if string_type(input_tofloat) != "float":
-        return "invalid input"
-    else:
-        return float(input_tofloat)
-    #end
+    
+    return float(input_tofloat)
+    
 #end
 
 def user_input(string_to_show, typemode):
